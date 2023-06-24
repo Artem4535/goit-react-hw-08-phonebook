072a3b8a7aa91768d846270702ddc3808b7fd107
Artem4535 <112252037+Artem4535@users.noreply.github.com>
2023-06-24 20:33:50 +0000

    Deploying to gh-pages from @ Artem4535/goit-react-hw-08-phonebook@c92449fe9d2248961317fb05c0462fe198794121 🚀

---
+++ static/js/60.da888e2e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[60],{3060:function(e,t,r){r.r(t);var c=r(184);t.default=function(){return(0,c.jsx)("h1",{children:"Welcome"})}}}]);
+//# sourceMappingURL=60.da888e2e.chunk.js.map
